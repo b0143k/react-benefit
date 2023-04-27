4cc58c4eeeefd28b6e606c8f802888447352fcb4
Bharat khandelwal <b0143k@gmail.com>
2023-04-28 03:41:32 +0530

    lucky-number

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Game from "./Lucky/Lucky.js";
+function App(){
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Benefits of using react ...</p>
-                <ol>
-                    <li>Component based architecture</li>
-                    <li>Virtual Dom for efficent updates</li>
-                    <li>Rich ecosystem and community</li>
-                    <li>cross-platform development </li>
-                    <li>Strong community support:</li>
-                </ol>
-                <button> Get started ...</button>
-            </header>
-        </div>
-    );
+        <Game/>
+    )
 }
 
 export default App;
