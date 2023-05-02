86258cec1d296113c6ab1d9e770be965b7280df7
Bharat khandelwal <b0143k@gmail.com>
2023-05-02 22:23:36 +0530

    Input-Effect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ImageChanger from './ImageChanger';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Benefits of using react ...</p>
-                <ol>
-                    <li>Component based architecture</li>
-                    <li>Virtual Dom for efficent updates</li>
-                    <li>Rich ecosystem and community</li>
-                    <li>cross-platform development </li>
-                    <li>Strong community support:</li>
-                </ol>
-                <button> Get started ...</button>
-            </header>
+     <ImageChanger />
         </div>
     );
 }
