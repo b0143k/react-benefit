380557f06e9bda283db1a5cae566b13b7b768183
Bharat khandelwal <b0143k@gmail.com>
2023-05-03 21:11:41 +0530

    Dog-Ass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import NewDogImg from "./NewDog"
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Benefits of using react ...</p>
-                <ol>
-                    <li>Component based architecture</li>
-                    <li>Virtual Dom for efficent updates</li>
-                    <li>Rich ecosystem and community</li>
-                    <li>cross-platform development </li>
-                    <li>Strong community support:</li>
-                </ol>
-                <button> Get started ...</button>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <NewDogImg/>
+    </div>
+  );
 }
 
 export default App;
