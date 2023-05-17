6f76fc91a95e0675e48430b8bec4917d959dacc3
Bharat khandelwal <b0143k@gmail.com>
2023-05-17 16:37:06 +0530

    add-delete_routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import React, { useState } from "react";
 
-import Todo from "./Components/todo";
 
 function App(){
-return(
+    const [users, setusers] = useState([]);
+    const [newuser,setNewUser] = useState('');
+
+
+    function handleDeleteUser(index){
+        const updatedUsers = [...users];
+        updatedUsers.splice(index, 1);
+        setusers(updatedUsers);
+};
+
+function handleAddUser(){
+    if (newuser.trim() !== ''){
+        setusers([...users, newuser]);
+        setNewUser('');
+    }
+};
+
+return (
     <div className="App">
-<Todo />
-
-
+        <h2>Add User'S</h2>
+        <ul>
+            {users.map((user,index) => (
+                <li key={index}>
+                    {user}{''}
+                    <button className="Delete-btn" onClick={()=> handleDeleteUser(index)}>Delete</button>
+                </li>
+            ))} 
+        </ul>
+        <input type="text" value={newuser} onChange={(event)=>setNewUser(event.target.value)} placeholder="Add new user" />
+        <button className="Add-btn" onClick={handleAddUser}>Add</button>
     </div>
-)
-}
-  export default App;
+    );
+            }
+
+            export default App;
